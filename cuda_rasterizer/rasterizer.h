d6624dab88b990ebd9fbaab7f162aded83a9ff57
Moritz Froese <mofro04@gmail.com>
2025-06-10 22:33:44 +0200

    add error_render to rasterizer.h/forward

--- cuda_rasterizer/rasterizer.h
+++ cuda_rasterizer/rasterizer.h
@@ -50,6 +50,7 @@ namespace CudaRasterizer
 			const float tan_fovx, float tan_fovy,
 			const bool prefiltered,
 			float* out_color,
+			float* error_render,
 			float* depth,
 			bool antialiasing,
 			int* radii = nullptr,
